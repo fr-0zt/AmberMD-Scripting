72d0819be402750dd4d089fe81169bc8dd08e8c7
Senal Liyanage <118088851+fr-0zt@users.noreply.github.com>
2023-07-12 22:38:11 -0500

    Automated multi-trajectory AMBER inputs
    This repository contains a bash script for automating the generation of input files for molecular dynamics simulations using AMBER. The script is designed to handle multiple trajectories, creating separate directories for minimization, heating, equilibration, and production stages of each simulation. It also applies ligand constraints based on a specified range of residues. The script is user-friendly, robust, and extensively commented for readability and ease of use. Users can easily adjust the number of trajectories and the range of residues for ligand constraints. This tool simplifies the process of setting up multiple simulations, making it particularly useful for high-throughput molecular dynamics studies.

---
+++ generate_amber_inputs.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+###########################################################
+# generate_amber_inputs.sh
+# Author: Senal Liyanage
+# Affiliation: Mississippi State University
+# Contact: sdd313@msstate.edu
+# Date: June 4, 2023
+# Description: Bash script for generating AMBER inputs for multiple trajectories
+###########################################################
+
+# Usage message
+usage() {
+    echo "USAGE: $0 [RESIDUE_START] [RESIDUE_END] [NUMBER_OF_TRAJECTORIES]"
+    echo "Generates AMBER scripts for the specified number of trajectories, with ligand constraints for the given range of residues."
+}
+
+# Check the number of arguments
+if [ $# -ne 3 ]; then
+    echo "Error: Incorrect number of arguments."
+    usage
+    exit 1
+fi
+
+# Check if arguments are numbers
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] || ! [[ $2 =~ $re ]] || ! [[ $3 =~ $re ]]; then
+    echo "Error: All arguments must be positive integers."
+    usage
+    exit 1
+fi
+
+# Set variables based on the arguments
+residue_start=$1
+residue_end=$2
+number_of_trajectories=$3
+amber_restraints="restraintmask=\'(:$residue_start-$residue_end & !@H=)\'"
+echo "Writing scripts for $number_of_trajectories trajectories with ligand constraints for residues $residue_start to $residue_end."
+
+# Create directories for minimization and heating
+directories=("001.min" "002.heat")
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "Error: Directory \'$dir\' already exists."
+        exit 1
+    fi
+    mkdir -p "$dir"
+done
+
+# Minimization scripts
+cat <<EOF > 001.min/minimize.in
+# Minimization with Cartesian restraints
+ &cntrl
+ imin=1,
+ maxcyc=10000,
+ ncyc=5000,
+ ntb=1,
+ cut=12.0,
+ ntr=1,
+ $amber_restraints,
+ restraint_wt=500
+/
+END
+
+EOF
+echo "" >> 001.min/minimize.in
+
+cat <<EOF > 001.min/minimize2.in
+# Minimization without Cartesian restraints
+ &cntrl
+ imin=1,
+ maxcyc=10000,
+ ncyc=5000,
+ ntb=1,
+ ntr=0,
+ cut=12.0
+/
+END
+
+EOF
+echo "" >> 001.min/minimize2.in
+
+# Heating scripts
+cat <<EOF > 002.heat/heat-with-constraints.in
+# Lipid 144 heating 100K for 5ps
+ &cntrl
+  imin=0,
+  ntx=1,
+  ntc=2,
+  ntf=2,
+  tol=0.0000001,
+  nstlim=2500,
+  ntt=3,
+  gamma_ln=1.0,
+  ntr=1,
+  ig=-1,
+  ntpr=100,
+  ntwr=10000,
+  ntwx=100,
+  dt=0.002,
+  nmropt=1,
+  ntb=1,
+  ntp=0,
+  cut=10.0,
+  ioutfm=1,
+  ntxo=2,
+  restraint_wt=10.0,
+  $amber_restraints
+ /
+ &wt type='TEMP0', istep1=0, istep2=2500,
+                   value1=0.0, value2=100.0 /
+ &wt type='END' /
+
+EOF
+echo "" >> 002.heat/heat-with-constraints.in
+
+cat <<EOF > 002.heat/heat-with-constraints2.in
+# Lipid 144 heating 310K for 195 ps
+ &cntrl
+  imin=0,
+  ntx=5,
+  irest=1,
+  ntc=2,
+  ntf=2,
+  tol=0.0000001,
+  nstlim=97500,
+  ntt=3,
+  gamma_ln=1.0,
+  ntr=1,
+  ig=-1,
+  ntpr=100,
+  ntwr=10000,
+  ntwx=100,
+  dt=0.002,
+  nmropt=1,
+  ntb=2,
+  ntp=2,
+  taup=2.0,
+  cut=10.0,
+  ioutfm=1,
+  ntxo=2,
+  restraint_wt=10.0,
+  $amber_restraints
+ /
+ &wt type='TEMP0', istep1=0, istep2=97500,
+                   value1=100.0, value2=310.0 /
+ &wt type='END' /
+
+EOF
+echo "" >> 002.heat/heat-with-constraints2.in
+
+# Create trajectory directories and the corresponding input files
+for ((traj=1; traj<=number_of_trajectories; traj++)); do
+    directories=("Traj${traj}/003.equil" "Traj${traj}/004.prod")
+    for dir in "${directories[@]}"; do
+        if [ -d "$dir" ]; then
+            echo "Error: Directory \'$dir\' already exists."
+            exit 1
+        fi
+        mkdir -p "$dir"
+    done
+
+    # Equilibration script
+    cat <<EOF > Traj${traj}/003.equil/equil.in
+    # Equilibration 5ns at 310K
+     &cntrl
+       imin=0, ntx=5, irest=1, 
+       ntc=2, ntf=2, tol=0.0000001,
+       nstlim=2500000, ntt=3, gamma_ln=1.0,
+       temp0=310.0,
+       ntpr=2500, ntwr=10000, ntwx=2500,
+       dt=0.002, ig=-1,
+       ntb=2, cut=10.0, ioutfm=1, ntxo=2,
+       ntp=2,
+     /
+     /
+     &ewald
+      skinnb=3.0,
+     /
+
+    EOF
+    echo "" >> Traj${traj}/003.equil/equil.in
+
+    # Production script
+    cat <<EOF > Traj${traj}/004.prod/production.in
+    # Production 10 ns NPT at 310K
+     &cntrl
+       imin=0, ntx=5, irest=1,
+       ntc=2, ntf=2, tol=0.0000001,
+       nstlim=5000000, ntt=3, gamma_ln=2.0,
+       temp0=310.0,
+       ntpr=2500, ntwr=10000, ntwx=2500,
+       dt=0.002, ig=-1,
+       ntb=2, cut=10.0, ioutfm=1, ntxo=2,
+       ntp=2, barostat=2,
+     /
+
+    EOF
+    echo "" >> Traj${traj}/004.prod/production.in
+done
