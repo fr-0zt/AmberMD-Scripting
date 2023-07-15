954f67619ccfedca536afe9e6869747a60bb2de4
Senal Liyanage <118088851+fr-0zt@users.noreply.github.com>
2023-07-15 14:56:39 -0500

    Update run_md_simulations.sh

--- run_md_simulations.sh
+++ run_md_simulations.sh
@@ -12,7 +12,7 @@
 # Usage message
 usage() {
     echo
-    echo "USAGE: run_md_simulations.sh [SYSTEM NAME] [CPU] [0/1] [GPU INDEX] [MD Start] [MD Stop]"
+    echo "USAGE: run_md_simulations.sh [SYSTEM NAME] [CPU] [0/1] [GPU INDEX] [MD Start] [MD Stop] [TRAJECTORY NUMBER]"
     echo
     echo "For preproduction run, set the third argument to 0."
     echo "For production run, set the third argument to 1."
@@ -34,6 +34,7 @@ simulation_type=$3
 gpu_index=$4
 md_start=$5
 md_stop=$6
+trajectory_number=$7
 
 # Function for preproduction run
 preproduction_run() {
@@ -48,38 +49,38 @@ preproduction_run() {
 
 # Function for production run
 production_run() {
-    if [ $# -lt 6 ]; then
-        echo "Please specify the GPU index, the starting MD run, and the ending MD run."
+    # Check if necessary arguments are available
+    if [ -z "$gpu_index" ] || [ -z "$md_start" ] || [ -z "$md_stop" ] || [ -z "$trajectory_number" ]; then
+        echo "Please specify the GPU index, the starting MD run, the ending MD run, and the trajectory number."
         exit
     fi
 
     # Select a CUDA device and run NPT MD simulations until the target simulation length is reached (nmax+1).
     export CUDA_VISIBLE_DEVICES=$gpu_index
 
-    for dir in "003.equil" "004.md"; do
-        cd $dir
+    cd Traj${trajectory_number}/003.equil || { echo "Could not change to directory Traj${trajectory_number}/003.equil"; exit 1; }
 
-        # Equilibrate the system with constraints.
-        mpirun -np $cpu_cores pmemd.MPI -O -p ../$system_name.parm7 -c ../002.heat/$system_name-heat2.rst -i equil.in -o $system_name-equil.log -inf $system_name-equil.info -x $system_name-equil.nc -r $system_name-equil.rst
 
-        cd ../004.md
+    # Equilibrate the system with constraints.
+    mpirun -np $cpu_cores pmemd.MPI -O -p ../../$system_name.parm7 -c ../../002.heat/$system_name-heat2.rst -i equil.in -o $system_name-equil.log -inf $system_name-equil.info -x $system_name-equil.nc -r $system_name-equil.rst
 
-        # Production
-        if [ $md_start == "0" ]; then
-            $AMBERHOME/bin/pmemd.cuda -O -p ../$system_name.parm7 -c ../003.equil/$system_name-equil.rst -i production.in -o $system_name-md0.log -inf $system_name-md0.info -x $system_name-md0.nc -r $system_name-md0.rst
-            nrun=1
-        else
-            nrun=$md_start
-        fi
+    cd ../004.prod
 
-        prev=$(expr $nrun - 1)
-        nmax=$md_stop
+    # Production
+    if [ $md_start == "0" ]; then
+        $AMBERHOME/bin/pmemd.cuda -O -p ../../$system_name.parm7 -c ../003.equil/$system_name-equil.rst -i production.in -o $system_name-md0.log -inf $system_name-md0.info -x $system_name-md0.nc -r $system_name-md0.rst
+        nrun=1
+    else
+        nrun=$md_start
+    fi
 
-        while [ $nrun -le $nmax ]; do
-            $AMBERHOME/bin/pmemd.cuda -O -p ../$system_name.parm7 -c $system_name-md$prev.rst -i production.in -o $system_name-md$nrun.log -inf $system_name-md$nrun.info -x $system_name-md$nrun.nc -r $system_name-md$nrun.rst
-            prev=$nrun
-            nrun=$(expr $nrun + 1)
-        done
+    prev=$(expr $nrun - 1)
+    nmax=$md_stop
+
+    while [ $nrun -le $nmax ]; do
+        $AMBERHOME/bin/pmemd.cuda -O -p ../../$system_name.parm7 -c $system_name-md$prev.rst -i production.in -o $system_name-md$nrun.log -inf $system_name-md$nrun.info -x $system_name-md$nrun.nc -r $system_name-md$nrun.rst
+        prev=$nrun
+        nrun=$(expr $nrun + 1)
     done
 }
 
